7cd769021b8e6feadee80f398815d05f83167fdb
Михаил Федоренко <Muxa_F@mail.ru>
2023-07-28 18:13:03 +0300

    Add workflows

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -152,6 +152,3 @@ DJOSER = {
                     'user_delete': ('rest_framework.permissions.IsAdminUser'),
                     },
 }
-
-# CORS_ORIGIN_ALLOW_ALL = True # удалить
-# CORS_URLS_REGEX = r'^/api/.*$' # удалить
