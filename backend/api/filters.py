3b137d54f2e0378e0bc60fae3fe1423fc53ff001
Михаил Федоренко <Muxa_F@mail.ru>
2023-08-05 14:20:15 +0300

    Update filters

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,5 +1,7 @@
 from django_filters.rest_framework import (AllValuesMultipleFilter,
-                                           FilterSet, ModelChoiceFilter)
+                                           BooleanFilter,
+                                           FilterSet,
+                                           ModelChoiceFilter)
 
 from recipes.models import Recipe, User
 from rest_framework.filters import SearchFilter
@@ -10,10 +12,24 @@ class RecipeFilter(FilterSet):
 
     tags = AllValuesMultipleFilter(field_name="tags__slug", label="tags")
     author = ModelChoiceFilter(queryset=User.objects.all())
+    is_favorited = BooleanFilter(method='filter_is_favorited')
+    is_in_shopping_cart = BooleanFilter(
+        method='filter_is_in_shopping_cart'
+    )
 
     class Meta:
         model = Recipe
-        fields = ("author", "tags",)
+        fields = ("author", "tags", "is_favorited", "is_in_shopping_cart")
+
+    def filter_is_favorited(self, queryset, name, value):
+        if value and self.request.user.is_authenticated:
+            return queryset.filter(favorite__user=self.request.user)
+        return queryset
+
+    def filter_is_in_shopping_cart(self, queryset, name, value):
+        if value and self.request.user.is_authenticated:
+            return queryset.filter(shopping_cart__user=self.request.user)
+        return queryset
 
 
 class IngredientFilter(SearchFilter):
